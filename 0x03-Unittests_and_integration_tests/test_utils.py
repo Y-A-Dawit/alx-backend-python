93025873e2268f1b23d8b6291bc8e60924afa1b3
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-07-20 22:30:28 +0300

    update fixes

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -55,15 +55,13 @@ class TestGetJson(unittest.TestCase):
         mock_response.json.return_value = test_payload
 
         with patch(
-            "utils.requests.get", return_value=mock_response
-            ) as mock_get:
+            "utils.requests.get", return_value=mock_response) as mock_get:
             result = get_json(
                     test_url
                     )
             mock_get.assert_called_once_with(
                     test_url
                     )
-            
             self.assertEqual(result, test_payload)
 
 
@@ -80,12 +78,11 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
         with patch.object(
-            TestClass, 'a_method', return_value=42
-            ) as mock_method:
-                obj = TestClass()
-                result1 = obj.a_property
-                result2 = obj.a_property
+                TestClass, 'a_method', return_value=42) as mock_method:
+            obj = TestClass()
+            result1 = obj.a_property
+            result2 = obj.a_property
 
-                self.assertEqual(result1, 42)
-                self.assertEqual(result2, 42)
-                mock_method.assert_called_once()
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            mock_method.assert_called_once()
