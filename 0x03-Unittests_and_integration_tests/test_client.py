2cdee3245dad2b099a834b690867150560d3dfd6
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-07-20 23:26:34 +0300

    UPDATE FIXES

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -38,26 +38,28 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch('client.get_json')
     def test_public_repos_url(self, mock_get_json):
         """
-        Test GithubOrgClient._public_repos_url returns 
+        Test GithubOrgClient._public_repos_url returns
         correct repos_url from org property.
         """
         with patch.object(
                 GithubOrgClient, 'org', new_callable=PropertyMock
                 ) as mock_org:
             # Setup the mock to return a dict with repos_url
-            mock_org.return_value = {"repos_url": "https://api.github.com/orgs/google/repos"}
+            mock_org.return_value = {
+                "repos_url": "https://api.github.com/orgs/google/repos"}
 
             client = GithubOrgClient("google")
             result = client._public_repos_url
             expected = "https://api.github.com/orgs/google/repos"
 
             self.assertEqual(result, expected)
+
     @patch('client.get_json')
+
     def test_public_repos(self, mock_get_json):
         """
-        Test GithubOrgClient.public_repos returns 
+        Test GithubOrgClient.public_repos returns
         list of repo names correctly."""
-        
         # Mock the return value of get_json to a list of repo dicts
         mock_get_json.return_value = [
             {"name": "repo1"},
@@ -72,14 +74,12 @@ class TestGithubOrgClient(unittest.TestCase):
                 GithubOrgClient, "_public_repos_url", new_callable=PropertyMock
                 ) as mock_url:
             mock_url.return_value = "https://api.github.com/orgs/some_org/repos"
-            
             # Call public_repos(), which uses the mocked _public_repos_url and get_json
             repos = client.public_repos()
-            
             # Expected list of repo names
             expected_repos = ["repo1", "repo2", "repo3"]
-            
             self.assertEqual(repos, expected_repos)
             mock_url.assert_called_once()
             mock_get_json.assert_called_once_with(
                 "https://api.github.com/orgs/some_org/repos")
+            
