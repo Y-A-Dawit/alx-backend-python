795ae0caccbcdb0e7ba4301d637656ece3f312af
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-07-27 21:41:07 +0300

    updating some fixes

--- messaging_app/chats/filters.py
+++ messaging_app/chats/filters.py
@@ -1,12 +1,12 @@
 import django_filters
-from .models import Message
+from chats.models import Message
 
 class MessageFilter(django_filters.FilterSet):
-    sender = django_filters.CharFilter(field_name='sender__user_id', lookup_expr='exact')
+    sender = django_filters.CharFilter(field_name='sender__user_id', lookup_expr='iexact')
     conversation = django_filters.CharFilter(field_name='conversation__conversation_id', lookup_expr='exact')
-    date_from = django_filters.DateTimeFilter(field_name='timestamp', lookup_expr='gte')
-    date_to = django_filters.DateTimeFilter(field_name='timestamp', lookup_expr='lte')
+    start_date = django_filters.DateTimeFilter(field_name="timestamp", lookup_expr='gte')
+    end_date = django_filters.DateTimeFilter(field_name="timestamp", lookup_expr='lte')
 
     class Meta:
         model = Message
-        fields = ['sender', 'conversation', 'date_from', 'date_to']
+        fields = ['sender', 'conversation','start_date', 'end_date']
