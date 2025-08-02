06319d9398e8e4b55de1880a44171fdb1d9c1e3a
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-08-02 03:57:51 +0300

    update on some bug fixes

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -8,7 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
-            'user_id', 'first_name', 'last_name',
+            'id', 'username'
             'email', 'phone_number', 'role', 'created_at', 'full_name'
         ]
 
